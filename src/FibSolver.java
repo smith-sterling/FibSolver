95bb817566a591885ed38361eb0c33eb102d27ce
smith-sterling <104526450+Viper-Fist@users.noreply.github.com>
2023-03-04 11:11:37 -0700

    push

--- src/FibSolver.java
+++ src/FibSolver.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class FibSolver {
+public class FibSolver {
+    public static long fib(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        long[][] base = {{1, 1}, {1, 0}};
+        long[][] res = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res = multiply(res, base);
+            }
+            n /= 2;
+            base = multiply(base, base);
+        }
+        return res[1][0];
+    }
+
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] c = new long[2][2];
+        c[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0];
+        c[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1];
+        c[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0];
+        c[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1];
+        return c;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void cout(Object o) { System.out.println(o); }
+    public static String cin() { return new Scanner(System.in).nextLine(); }
+
+    public static void main (String[] args) {
+        cout("""
+                \nWelcome to Fib Solver 2000! Enter a number, 'n' to get the nth value in the fibonacci sequence!
+                (or a negative number to quit)""");
+        int n;
+        long answer = 1;
+        while(answer >= 0) {
+            String s = cin();
+            try {
+                n = Integer.parseInt(s);
+                if (n < 0) break;
+                if (n > 91) {
+                    cout("Sorry, that's a little too big for me to handle right now...");
+                    cout("Mainly because that number is larger than I can store, but hey");
+                    continue;
+                }
+                answer = FibSolver.fib(n);
+                cout(String.format("%,d", answer));
+                cout("\nEnter another number?");
+            } catch (NumberFormatException e) {
+                cout("That's not a number!");
+            }
+        }
+    }
 }
